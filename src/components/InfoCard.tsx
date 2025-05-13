397f2157a08860d0f595a2698452ca085d25bb61
Morgan Yeh <fs.morganyeh@gmail.com>
2025-05-12 20:27:02 -0400

    update InfoCard.tsx

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -11,7 +11,7 @@ export default function InfoCard({msg, imgName} : InfoCardProps) {
             <div className="text-container">
                 <h1 id="result">{msg}</h1>
             </div>
-            <img src={"/src/assets/" + imgName + ".jpg"} alt={imgName} id="kanade-sign"></img>
+            <img src={"/assets/" + imgName + ".jpg"} alt={imgName} id="kanade-sign"></img>
         </div>
     </>)
 }
