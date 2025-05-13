8a62967c8ae6d5b09c8dc4e1e7992b43698197b4
Morgan Yeh <fs.morganyeh@gmail.com>
2025-05-12 20:01:24 -0400

    revert base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/monday-reminder/dist/",
+  base: "/monday-reminder/",
 })
